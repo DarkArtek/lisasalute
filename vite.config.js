b75bfadbe7900f87572d62459c715f7929d98d7b
Artek <darkartek@gmail.com>
2025-11-25 20:50:01 +0100

    fis: incrementing chunk limit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    // Aumentiamo il limite dell'avviso a 2MB (2000kB) per accomodare jsPDF
+    chunkSizeWarningLimit: 2000,
+  },
 })
